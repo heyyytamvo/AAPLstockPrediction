1f865d4f4f74367540e6986abf1776b5df7cbe2f
heyyytamvo <tamvoforwork@gmail.com>
2024-04-16 16:55:17 +0700

    Ready to deploy production

--- app.py
+++ app.py
@@ -5,32 +5,7 @@ import pandas as pd
 import parameters
 from apscheduler.schedulers.background import BackgroundScheduler
 
-# Global flag variable to track whether the function has already run
-global firstRun
-firstRun = False
-
-def my_startup_function():
-    # Your startup function code here
-    # Your function code here
-    global firstRun
-    '''DEBUG'''
-    print("Call my StartUp Function")
-    print(f"!!!!!!!!!!!!{firstRun}")
-    '''DEBUG'''
-    
-    if firstRun == False:
-        
-        _dataLoader = dataLoader()
-        _predictor = Predictor()
-
-        x_data = _dataLoader.getScaledCloseData()
-        _predictor.predict(x_data)
-        firstRun = True
-
 def ApplePrediction():
-    '''DEBUG'''
-    print("Automatically call ApplePrediction")
-    '''DEBUG'''
     # Your function code here
     _dataLoader = dataLoader()
     _predictor = Predictor()
@@ -41,12 +16,30 @@ def ApplePrediction():
 # Create a scheduler
 scheduler = BackgroundScheduler()
 # Add the scheduled job to the scheduler
-scheduler.add_job(ApplePrediction, 'cron', hour=9)
+scheduler.add_job(ApplePrediction, 'cron', hour=9, minute=30)
 # Start the scheduler
 scheduler.start()
-
 # Create a Flask app
 app = Flask(__name__)
+# Global flag variable to track whether the function has already run
+global firstRun
+firstRun = False
+
+def my_startup_function():
+    # Your startup function code here
+    # Your function code here
+    global firstRun
+
+    
+    if firstRun == False:
+        
+        _dataLoader = dataLoader()
+        _predictor = Predictor()
+
+        x_data = _dataLoader.getScaledCloseData()
+        _predictor.predict(x_data)
+        firstRun = True
+
 
 @app.route('/home', methods=['GET'])
 def home():
@@ -55,11 +48,6 @@ def home():
 # Define a route for the API endpoint
 @app.route('/api/apple', methods=['GET'])
 def Apple():
-    # _dataLoader = dataLoader()
-    # _predictor = Predictor()
-
-    # x_data = _dataLoader.getScaledCloseData()
-    # result = _predictor.predict(x_data)
     df = pd.read_csv("History.txt")
     currentDate = parameters.currentDate
     currentWeekDate = parameters.currentWeekDate
@@ -70,7 +58,8 @@ def Apple():
             'Ticker': 'Apple',
             'Date' : currentDate,
             'Weekdate' : currentDate,
-            'Predicted Close Price of Today' : str(df["Predicted Close Price"].values[-1])
+            'Predicted Close Price of Today' : float(df["Predicted Close Price"].values[-1]),
+            'message' : 'success'
         }
         
     else:
@@ -78,7 +67,8 @@ def Apple():
             'Ticker': 'Apple',
             'Date' : currentDate,
             'Weekdate' : currentDate,
-            'Predicted Close Price of Next Monday' : str(df["Predicted Close Price"].values[-1])
+            'Predicted Close Price of Next Monday' : float(df["Predicted Close Price"].values[-1]),
+            'message' : 'success'
         }
     
     # Convert the data to JSON format and return it
@@ -92,7 +82,8 @@ def AppleHistory():
         'Ticker': 'Apple',
         'Actual' : list(df["Actual Close Price"].values),
         'Predicted' : list(df["Predicted Close Price"].values),
-        'Date': list(df['Date'].values)
+        'Date': list(df['Date'].values),
+        'message' : 'success'
     }
     
     # Convert the data to JSON format and return it
@@ -100,5 +91,5 @@ def AppleHistory():
 
 
 if __name__ == '__main__':
-    # my_startup_function()
-    app.run(host="0.0.0.0", port=3000,debug=True)
+    my_startup_function()
+    app.run(host="0.0.0.0", port=3000,debug=False)
